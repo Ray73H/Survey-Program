7e9841608e627a661c5b18e58cbbf0fa9f16ab10
Ray Heinonen <Ray_heinonen@hotmail.com>
2025-05-09 18:47:02 +0200

    Fixed server port and connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import { config } from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import surveyRoutes from "./routes/surveyRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -7,6 +8,7 @@ import userRoutes from "./routes/userRoutes.js";
 config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 connectDB();
@@ -14,7 +16,7 @@ connectDB();
 app.use("/api/surveys", surveyRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
