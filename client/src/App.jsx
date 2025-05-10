fe09454d44f53f7fb4ad909b52309d712b5ad3f7
creichenbach <creichenbach@student.ethz.ch>
2025-05-10 18:51:21 +0200

    #1 initial login pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ExperimenterLayout from "./layouts/ExperimenterLayout";
 import ExperimenteeLayout from "./layouts/ExperimenteeLayout";
+import LoginLayout from "./layouts/LoginLayout";
 import SurveyBuilder from "./pages/SurveyBuilder";
 import Experimenter from "./pages/Experimenter";
 import Experimentee from "./pages/Experimentee";
 import JoinSurvey from "./pages/JoinSurvey";
-
+import ExperimenterLogin from "./pages/ExperimenterLogin"
+import ExperimenteeLogin from "./pages/ExperimenteeLogin";
+import SuperUserLogin from "./pages/SuperUserLogin";
 
 
 
@@ -14,7 +17,11 @@ function App() {
         <Router>
             <Routes>
                 {/* Public route */}
-
+                <Route element={<LoginLayout />}>
+                    <Route path ="/experimenterlogin" element={<ExperimenterLogin />} />
+                    <Route path ="/experimenteelogin" element={<ExperimenteeLogin />} />
+                    <Route path ="/superlogin" element={<SuperUserLogin />} />
+                </Route>
 
                 {/* Experimenter routes */}
                 <Route element={<ExperimenterLayout />}>
